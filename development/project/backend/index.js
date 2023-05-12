ceb556e41813969c16379614442ed57be17d6a34
Yan M. Coutinho <yan.m.coutinho@gmail.com>
2023-05-12 11:12:40 -0300

    Joined all endpoints

--- development/project/backend/index.js
+++ development/project/backend/index.js
@@ -3,8 +3,7 @@ const DBManager = require('./classes/DBManager.js');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const sqlite3 = require('sqlite3').verbose();
-const DBPATH = 'data/test.db';
+const DBPATH = 'data/project.db';
 const DBM = new DBManager(DBPATH)
 
 const hostname = '127.0.0.1';
@@ -13,18 +12,6 @@ const app = express();
 
 
 app.use(express.json());
-/**
- * 1. SELECT * FROM tb_tabela LIMIT 1, limite_recebido
-2. SELECT * FROM tb_tabela WHERE id_tabela = id_recebido_tabela
-3. INSERT INTO tb_report(dados_databela) VALUES (dados_recebidos)
-ADM
-4. SELECT * FROM tb_report WHERE concluida=false
-5. SELECT * FROM tb_report WHERE id_report = id_recebido
-6. UPDATE tb_req SET concluida=true WHERE id_report = id_recebido
-7. UPDATE tb_campos SET campo=''" WHERE id_tabela = id_recebido
-8. SELECT * FROM tb_report WHERE concluida=false
-9. INSERT INTO tb_tabela(dados_tabela)  VALUES (dados_nova_tabela)
- */
 
 
 /**
@@ -38,34 +25,290 @@ ADM
  */
 
 /**
+ * Endpoint that insert new data in TB_REQUISICAO
+ * @param {Object} data 
+ */
+app.post('/insert-req', async (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	let data = JSON.parse(req.body.data);
+	
+	for (let request of data) {
+		let columns = ['ID_TABELA', 'USUARIO', 'ID_STATUS'];
+		let values = [
+			request['id_tabela'], request['user'], 
+			request['status']
+		];
+		DBM.insertReturningTheInsertedDataID("TB_REQUISICAO", columns, values).then(async (result) => {
+			let tbRequisicaoId = result[0];
+			for (let reqsTabela of request["reqs_tabela"]) {
+				columns = ["ID_REQUISICAO", "ID_CAMPO_TABELA", "ALTERACAO"];
+				values = [tbRequisicaoId, reqsTabela['id_campo_tabela'], reqsTabela['alteracao']];
+				await DBM.insert("TB_REQ_TABELA", columns, values);
+			}
+
+			for (let reqsVariavel of request["reqs_variavel"]) {
+				columns = ["ID_REQUISICAO", "ID_CAMPO_VARIAVEL", "ALTERACAO"];
+				values = [tbRequisicaoId, reqsVariavel['id_campo_variavel'], reqsVariavel['alteracao']];
+				await DBM.insert("TB_REQ_VARIAVEL", columns, values);
+			}
+
+			for (let reqsConexao of request["reqs_conexao"]) {
+				columns = ["ID_REQUISICAO", "ID_CAMPO_CONEXAO", "ALTERACAO"];
+				values = [tbRequisicaoId, reqsConexao['id_campo_conexao'], reqsConexao['alteracao']];
+				await DBM.insert("TB_REQ_CONEXAO", columns, values);
+			}
+		});
+	}
+	res.end();
+});
+
+
+/**
+ * Endpoint that insert new data in TB_TABELA
+ * @param {Object} data 
+ */
+app.post('/insert-tb', async (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	let data = JSON.parse(req.body.data);
+
+	for (let insert of data) {
+		let columns = [
+			"ID", "CONJUNTODADOS_PRODUTO", "ID_TABELA", "TABELA", "CONTEUDO_TABELA", 
+			"CRITICIDADE_TABELA", "DADOS_SENSIVEIS", "DEFASAGEM", "DATABASE", "CAMINHO", 
+			"SCRIPTS_ALIMENTACAO","ENG_INGESTAO", "OWNER", "STEWARD","INDICADORAJUSTENOMENCLATURATABELA", 
+			"LINK_SOL_ACESSO", "LINK_REPORTAR_ERRO","RANKING_GOVERNANCA", "QTD_VIZUALIZACAO" 
+		];
+		
+		let values = [
+			insert['ID'], insert['CONJUNTODADOS_PRODUTO'], insert['ID_TABELA'], insert['TABELA'], 
+			insert['CONTEUDO_TABELA'], insert['CRITICIDADE_TABELA'], insert['DADOS_SENSIVEIS'], insert['DEFASAGEM'], 
+			insert['DATABASE'], insert['CAMINHO'], insert['SCRIPTS_ALIMENTACAO'], insert['ENG_INGESTAO'], 
+			insert['OWNER'], insert['STEWARD'], insert['INDICADORAJUSTENOMENCLATURATABELA'], insert['LINK_SOL_ACESSO'],
+			insert['LINK_REPORTAR_ERRO'], insert['RANKING_GOVERNANCA'], insert['QTD_VIZUALIZACAO']
+		];
+		
+		await DBM.insert("TB_TABELA", columns, values).then(async (results) => {	
+			let tbTabelaId = insert['ID'];
+			for (let TbConexao of insert["CONEXAO"]) {
+				columns = [
+					"ID", "CONJUNTODADOS_PRODUTO", "TABELA", "ORDEM_ORIGEM", "TABELA_ORIGEM", "SISTEMA_ORIGEM", "SERVIDOR_ORIGEM", 
+					"DATABASE_ORIGEM", "SCHEMA_ORIGEM", "TIPO_CONEXAO", "REPOSITORIO", "MECANICA", "FREQUENCIA", "MODO_ESCRITA"
+				];
+
+				values = [
+					tbTabelaId, insert['CONJUNTODADOS_PRODUTO'], insert['TABELA'], TbConexao['ORDEM_ORIGEM'], 
+					TbConexao['TABELA_ORIGEM'], TbConexao['SISTEMA_ORIGEM'], TbConexao['SERVIDOR_ORIGEM'], TbConexao['DATABASE_ORIGEM'],
+					TbConexao['SCHEMA_ORIGEM'], TbConexao['TIPO_CONEXAO'], TbConexao['REPOSITORIO'], TbConexao['MECANICA'], 
+					TbConexao['FREQUENCIA'], TbConexao['MODO_ESCRITA']
+				];
+
+				await DBM.insert("TB_CONEXAO", columns, values);
+			}
+
+			for (let TbVariavel of insert["VARIAVEL"]) {
+				columns = [
+					"CONJUNTO_DADOS", "TABELA", "NOME_CAMPO", "TIPO_CAMPO", "TIPO_PESSOA", "DESCRICAO_CAMPO", "VOLATIL", 
+					"CH_PRIMARIA", "CH_PRIMARIA", "NULL", "UNQ", "LGPD"
+				];
+					
+				values = [
+					insert['CONJUNTODADOS_PRODUTO'], tbTabelaId, TbVariavel['NOME_CAMPO'], TbVariavel['TIPO_CAMPO'], 
+					TbVariavel['TIPO_PESSOA'], TbVariavel['DESCRICAO_CAMPO'], TbVariavel['VOLATIL'], TbVariavel['CH_PRIMARIA'],
+					TbVariavel['NULL'], TbVariavel['UNQ'], TbVariavel['LGPD']
+				];
+			
+				await DBM.insert("TB_VARIAVEL", columns, values);
+			}
+
+			for (let TbClassificacao of insert["CLASSIFICACAO"]) {
+				columns = [
+					"ID_TABELA", "ID_VALOR_CLASSIFICACAO"
+				];
+
+				values = [
+					tbTabelaId, TbClassificacao['ID_VALOR_CLASSIFICACAO']
+				];
+
+				await DBM.insert("TB_CLASSIFICACAO_TABELA", columns, values);
+			}
+		});
+	}
+	res.end();
+});
+
+
+/**
  * SELECT METHODS (R)
+ */
+
+/**
+ * Endpoint that search data in TB_TABELA and it's children
+ * @param {String} q
+ * @param {Integer} index
+ * @param {Integer} maxRows  
+ * @returns {JSON}
  */
 app.get('/search', async (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	let search = req.query.search;
+	let q = req.query.q;
 	let index = req.query.index;
 	let maxRows = req.query.maxRows;
-
-	let sql = `select * from TB_TABELA limit ${index}, ${maxRows} where TABELA like "%?%"`;
-	await DBM.select(sql, [search]).then((result) => {
+	let value = "%" + q + "%";
+	let sql = `select * from TB_TABELA where TABELA like ? limit ${index}, ${maxRows}`;
+	await DBM.select(sql, [value]).then((result) => {
 		res.json(result);
 	});
 });
 
 
+/**
+ * Endpoint that search data in TB_TABELA and it's children
+ * @param {String} id
+ * @returns {JSON}
+ */
 app.get('/table', async (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	let id = req.query.id;
+	let sql = "select * from TB_TABELA " +
+		"inner join TB_OWNER_STEWARD on " +
+		"TB_TABELA.CONJUNTODADOS_PRODUTO = TB_OWNER_STEWARD.ConjuntodeDados " +
+		"where ID=?";
+	await DBM.select(sql, [id]).then(async (result) => {
+		let response = result[0];
+		response["CONEXAO"] = await DBM.select("select * from TB_CONEXAO where ID=?", [id]);
+		response["VARIAVEL"] = await DBM.select("select * from TB_VARIAVEL where TABELA=?", [id]);
+		sql = "select * from TB_CLASSIFICACAO_TABELA where ID_TABELA=?";
+		response["CLASSIFICACAO_TABELA"] = await DBM.select(sql, [id]);		
+		res.json(response);
+	});
 });
+
+
+/**
+ * Endpoint that search data in TB_REQUISICAO where ID_STATUS is equal to the status argument
+ * @param {Integer} status
+ * @returns {JSON}
+ */
+app.get('/requests', async (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	let status = req.query.status;
+	let sql = "select * from TB_REQUISICAO" + " where STATUS=?";
+	await DBM.select(sql, [status]).then(async (result) => {
+		let response = result[0];
+		let id = response["ID_REQUISICAO"];
+		response["CONEXAO"] = await DBM.select("select * from TB_REQ_CONEXAO where ID_REQUISICAO=?", [id]);
+		response["VARIAVEL"] = await DBM.select("select * from TB_REQ_VARIAVEL where ID_REQUISICAO=?", [id]);
+		response["TABELA"] = await DBM.select("select * from TB_REQ_TABELA where ID_REQUISICAO=?", [id]);	
+		res.json(response);
+	});
+});
+
+
+/**
+ * Endpoint that search data in TB_REQUISICAO where ID_REQUISICAO is equal to the id argument
+ * @param {Integer} id
+ * @returns {JSON}
+ */
+app.get('/request', async (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	let id = req.query.id;
+	let sql = "select * from TB_REQUISICAO" + " where ID_REQUISICAO=?";
+	await DBM.select(sql, [id]).then(async (id) => {
+		let response = result[0];
+		response["CONEXAO"] = await DBM.select("select * from TB_REQ_CONEXAO where ID_REQUISICAO=?", [id]);
+		response["VARIAVEL"] = await DBM.select("select * from TB_REQ_VARIAVEL where ID_REQUISICAO=?", [id]);
+		response["TABELA"] = await DBM.select("select * from TB_REQ_TABELA where ID_REQUISICAO=?", [id]);	
+		res.json(response);
+	});
+});
+
+
 /**
  * UPDATE METHODS (U)
  */
 
+/**
+ * Endpoint that update the TB_REQUISICAO
+ * @param {Object} data
+ */
+app.post('/update-req', urlencodedParser, async(req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	let data = JSON.parse(req.body.data);
+
+	for(let requestUpdate in data) {
+		let idRequest = requestUpdate["id_requisicao"];
+		let columnsAndValues = {
+			"ID_STATUS": requestUpdate["status"]
+		};
+		await DBM.update("TB_REQUISICAO",	columnsAndValues, ("ID_REQUISICAO="+idRequest)).then(() => {
+			for(let reqConexao in requestUpdate["tb_req_conexao"]) {
+				columnsAndValues = {
+					"ALTERACAO_GOVERNANCA": reqConexao["alteracao_governanca"]
+				};
+				DBM.update("TB_REQ_CONEXAO", columnsAndValues, ("ID_REQ_CONEXAO="+reqConexao["id_req_conexao"]));
+			}
+
+			for(let reqTabela in requestUpdate["tb_req_tabela"]) {
+				columnsAndValues = {
+					"ALTERACAO_GOVERNANCA": reqTabela["alteracao_governanca"]
+				};
+				DBM.update("TB_REQ_TABELA", columnsAndValues, ("ID_REQ_TABELA="+reqTabela["id_req_tabela"]));
+			}
+
+			for(let reqVariavel in requestUpdate["tb_req_variavel"]) {
+				columnsAndValues = {
+					"ALTERACAO_GOVERNANCA": reqVariavel["alteracao_governanca"]
+				};
+				DBM.update("TB_REQ_VARIAVEL", columnsAndValues, ("ID_REQ_TABELA="+reqVariavel["id_req_variavel"]));
+			}
+		});
+	}
+
+	/**
+	 * Update-req receives a JSON with this pattern:
+	 * [
+	 * 		{
+	 * 			"id_requisicao": 1,
+	 * 			"status": 1,
+	 * 			"tb_req_conexao": [
+	 * 				{
+	 * 					"id_req_conexao": 1,
+	 * 					"alteracao_governanca": "algo"
+	 * 				}
+	 * 			],
+	 * 			"tb_req_tabela": [
+	 * 				{
+	 * 					"id_req_tabela": 1,
+	 * 					"alteracao_governanca": "algo"
+	 * 				}
+	 * 			],
+	 * "		tb_req_variavel": [
+	 * 				{
+	 * 					"id_req_variavel": 1,
+	 * 					"alteracao_governanca": "algo"
+	 * 				}
+	 * 			],
+	 *		}
+	 * ]
+	 */
+})
+
+
 
 /**
  * DELETE METHODS (D)
+ */
+
+/**
+ * Endpoint that delete data in the TB_TABELA
+ * @param {Integer} id
  */
 app.post('/remove-table', urlencodedParser, async(req, res) => {
 	res.statusCode = 200;
@@ -80,6 +323,10 @@ app.post('/remove-table', urlencodedParser, async(req, res) => {
 });
 
 
+/**
+ * Endpoint that delete data in the TB_CONEXAO
+ * @param {Integer} id
+ */
 app.post('/remove-connection', urlencodedParser, async(req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
@@ -89,6 +336,10 @@ app.post('/remove-connection', urlencodedParser, async(req, res) => {
 });
 
 
+/**
+ * Endpoint that delete data in the TB_VARIAVEL
+ * @param {Integer} id
+ */
 app.post('/remove-variable', urlencodedParser, async(req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
@@ -98,6 +349,10 @@ app.post('/remove-variable', urlencodedParser, async(req, res) => {
 });
 
 
+/**
+ * Endpoint that delete data in the TB_CLASSIFICACAO_TABELA
+ * @param {Integer} id
+ */
 app.post('/remove-classification', urlencodedParser, async(req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); 
